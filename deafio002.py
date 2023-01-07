6a3204712fa3b344457461ff71519f56d9e86640
Renato Augusto <ragoncalves@outlook.com>
2023-01-07 14:26:01 -0300

    Desafio 002
    Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas

---
+++ deafio002.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas
+
+dia = input('Dia: ')
+mes = input('Mês: ')
+ano = input('Ano: ')
+print('Você nasceu no dia {} do {} de {}. Correto?'.format(dia, mes, ano))
