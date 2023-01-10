59fdb0c5802411624d613cc7aa48f68f566e95a3
Renato Augusto <ragoncalves@outlook.com>
2023-01-10 15:17:10 -0300

    create desafio005.py

---
+++ desafio005.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
+
+n = int(input('Digite um número: '))
+anterior = n - 1
+sucessor = n + 1
+print(f'Anterior: {anterior} \nSucessor: {sucessor}')
