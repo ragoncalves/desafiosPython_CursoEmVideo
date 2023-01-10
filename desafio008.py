401ab65c6f36aa379d81d7767c2008edbcf731cc
Renato Augusto <ragoncalves@outlook.com>
2023-01-10 15:20:24 -0300

    create desafio008.py

---
+++ desafio008.py
@@ -0,0 +1,5 @@
+# Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros
+
+metros = float(input('Digite o valor em metros: '))
+print('Centímetros: {}'.format(metros * 100))
+print('Milímetros: {}'.format(metros * 1000))
