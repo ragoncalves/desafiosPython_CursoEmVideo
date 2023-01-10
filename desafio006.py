0ca7644643bd7d320402fdcd3a21d4e771360e8b
Renato Augusto <ragoncalves@outlook.com>
2023-01-10 15:18:22 -0300

    create desafio006.py

---
+++ desafio006.py
@@ -0,0 +1,6 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada
+
+n = int(input('Digite um número: '))
+print('Dobro: {}'.format(n * 2))
+print('Triplo: {}'.format(n * 3))
+print('Raíz Quadrada: {:.2f}'.format(n ** (1/2)))
