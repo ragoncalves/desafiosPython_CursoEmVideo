0a54287bb07690e54c27797d3b3be22c91add4ce
Renato Augusto <ragoncalves@outlook.com>
2023-01-10 15:19:35 -0300

    create desafio007.py

---
+++ desafio007.py
@@ -0,0 +1,7 @@
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média
+
+n1 = float(input('Primeira nota: '))
+n2 = float(input('Segunda nota: '))
+media = (n1 + n2) / 2
+
+print(f'Média: {media}')
