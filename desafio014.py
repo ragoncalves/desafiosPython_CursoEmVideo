40cb9d0436783f55e100bc7bb4179b9ae95adde0
Renato Augusto <ragoncalves@outlook.com>
2023-01-12 17:52:05 -0300

    Create desafio014 e 015.py

---
+++ desafio014.py
@@ -0,0 +1,4 @@
+# Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit
+
+c = float(input('Temperatura em graus celsius (ºC): '))
+print(f'A temperatura de {c:.2f} ºC corresponde a {(c*9/5) + 32:.2f} ºF!')
