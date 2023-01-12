7fb650eec45d4a23dd3ac18594f08755b3bfee4e
Renato Augusto <ragoncalves@outlook.com>
2023-01-12 17:50:11 -0300

    Create desafio013.py

---
+++ desafio013.py
@@ -0,0 +1,4 @@
+# Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento
+
+salario = float(input('Salário: R$'))
+print('Novo salário: R${:.2f}'.format(salario * 1.15))
