2af3451ed62bfba8963283bdc3f2704f4ac02ef1
Renato Augusto <ragoncalves@outlook.com>
2023-01-18 13:05:05 -0300

    Create desafio019.py

---
+++ desafio019.py
@@ -0,0 +1,12 @@
+# Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido
+
+from random import choice
+
+a1 = input('Primeiro aluno: ')
+a2 = input('Segundo aluno: ')
+a3 = input('Terceiro aluno: ')
+a4 = input('Quarto aluno: ')
+
+lista = [a1, a2, a3, a4]
+
+print(f'Escolhido: {choice(lista)}')
