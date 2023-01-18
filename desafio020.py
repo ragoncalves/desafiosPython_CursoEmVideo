f8be12a95a961a47be2bc071ef7afe0fb4feb0a5
Renato Augusto <ragoncalves@outlook.com>
2023-01-18 13:05:40 -0300

    Create desafio020.py

---
+++ desafio020.py
@@ -0,0 +1,14 @@
+# O mesmo professor do desafio 19 quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada
+
+from random import shuffle
+
+a1 = input('Primeiro aluno: ')
+a2 = input('Segundo aluno: ')
+a3 = input('Terceiro aluno: ')
+a4 = input('Quarto aluno: ')
+
+lista = [a1, a2, a3, a4]
+
+shuffle(lista)
+
+print(f'A ordem será: {lista}')
